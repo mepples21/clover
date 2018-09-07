a845f6686c35b4b1ba7fd10c2d9861e197769849
Michael Epping <michael@rvrwest.com>
2018-09-07 16:35:23 -0500

    Updated

--- Remove-americancommunications.com.ps1
+++ Remove-americancommunications.com.ps1
@@ -1,57 +1,11 @@
-<#
-
-.SYNOPSIS
-Removes email addresses from relevant domains so that the domains may be moved over to the new Office 365 tenant.
-
-.DESCRIPTION
-
-
-.EXAMPLE
-
-
-.LINK
-
-
-.NOTES
-Version: 1.0
-Date: 03/30/2017
-Authors: Michael Epping (mepping@concurrency.com)
-
-Wishlist
-    - Better error checking and handling
-
-/#>
-
-#Inputs
-
-
-
-#Define functions
-
-
-#Execute script
-
-
-#Create Output File
-$LogFileName = "Remove-EmailAddresses-$(Get-date -f yyyy-MM-dd-ss).log"
-New-Item -Path $LogFileName
-Start-Transcript -Path $LogFileName
-
-
-#Check for Exchange PowerShell
-$ExchangePowerShell = Get-Mailbox | Select-Object -First 1
-if ($ExchangePowerShell -eq $null) {
-    Write-Host "ERROR: This script must be run from the on-premises Exchange Management Shell." -Foreground Red
-    Exit(1)
-}
-
 #Remove email addresses from domains that are not supported
 
     #Put domains to remove in an array
 
     #Gather data
     $remotemailboxes = @()
-    $remotemailboxes = Get-RemoteMailbox -ResultSize Unlimited
+  #  $remotemailboxes = Get-RemoteMailbox -ResultSize Unlimited
+    $remotemailboxes = telecomone
 
     #Remove email addresses for each domain
     foreach ($object in $remotemailboxes) {
@@ -65,5 +19,3 @@ if ($ExchangePowerShell -eq $null) {
                 $emailaddresses = $emailaddresses - $badaddress;
                 $object | set-remotemailbox -emailaddresses $emailaddresses;
     }
-
-Stop-Transcript
--- Remove-go-ac.com.ps1
+++ Remove-go-ac.com.ps1
@@ -1,50 +1,3 @@
-<#
-
-.SYNOPSIS
-Removes email addresses from relevant domains so that the domains may be moved over to the new Office 365 tenant.
-
-.DESCRIPTION
-
-
-.EXAMPLE
-
-
-.LINK
-
-
-.NOTES
-Version: 1.0
-Date: 03/30/2017
-Authors: Michael Epping (mepping@concurrency.com)
-
-Wishlist
-    - Better error checking and handling
-
-/#>
-
-#Inputs
-
-
-
-#Define functions
-
-
-#Execute script
-
-
-#Create Output File
-$LogFileName = "Remove-EmailAddresses-$(Get-date -f yyyy-MM-dd-ss).log"
-New-Item -Path $LogFileName
-Start-Transcript -Path $LogFileName
-
-
-#Check for Exchange PowerShell
-$ExchangePowerShell = Get-Mailbox | Select-Object -First 1
-if ($ExchangePowerShell -eq $null) {
-    Write-Host "ERROR: This script must be run from the on-premises Exchange Management Shell." -Foreground Red
-    Exit(1)
-}
-
 #Remove email addresses from domains that are not supported
 
     #Put domains to remove in an array
@@ -65,5 +18,3 @@ if ($ExchangePowerShell -eq $null) {
                 $emailaddresses = $emailaddresses - $badaddress;
                 $object | set-remotemailbox -emailaddresses $emailaddresses;
     }
-
-Stop-Transcript
